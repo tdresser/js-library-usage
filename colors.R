1b5845bcf0974d58311ceed7f7800c9468396b90
Tim Dresser <tdresser@chromium.org>
2018-06-08 09:26:21 -0400

    Add graphing logic

---
+++ colors.R
@@ -0,0 +1,48 @@
+# From http://tools.medialab.sciences-po.fr/iwanthue/
+
+# From http://tools.medialab.sciences-po.fr/iwanthue/
+
+colors <- c(
+  "#d37181",
+  "#50da4e",
+  "#6a38cd",
+  "#8dd443",
+  "#c14cd3",
+  "#b0c540",
+  "#726ce2",
+  "#ddba36",
+  "#563193",
+  "#57a33f",
+  "#db44a8",
+  "#5acf90",
+  "#d23a69",
+  "#66ccc4",
+  "#e04734",
+  "#558ac8",
+  "#d57e31",
+  "#6775c8",
+  "#907f2c",
+  "#d990da",
+  "#366727",
+  "#81246a",
+  "#a6c984",
+  "#4e3e75",
+  "#d6b96e",
+  "#a95f9b",
+  "#718b54",
+  "#b0a4d9",
+  "#993a27",
+  "#91c4e3",
+  "#714a23",
+  "#5a8da6",
+  "#db9675",
+  "#494a61",
+  "#b0bba2",
+  "#742f3f",
+  "#bac2c0", 
+  "#d1a7b7",
+  "#3e472f",
+  "#927267"
+)
+
+
---
+++ main.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(plotly)
+
+source("colors.R")
+
+df <- read_csv('libs.csv') %>%
+  filter(date > '2017-05-22')
+
+first_date = first(df$date)
+last_date = last(df$date)
+
+deltas <- df %>%
+  group_by(library, client) %>%
+  summarize(first_value=min(frequency),
+           last_value=max(frequency), 
+           total_frequency=sum(frequency)) %>%
+  mutate(delta = last_value-first_value,
+         delta_percent = 100 * (last_value - first_value) / first_value)
+
+top_n = deltas %>% arrange(desc(total_frequency)) %>% head(n=20)
+
+# We care about libraries with highly variable popularity, which exceed a minimum popularity bar.
+# Or just libraries which are generally of interest.
+interesting_sites <- inner_join(df, deltas) %>%
+  filter((abs(delta_percent) > 40 & frequency > 150) |
+         library %in% c("Polymer", "Google Maps", "React"))
+
+interesting_sites %<>% mutate(text = sprintf("Date: %s<br>Library: %s<br>Page Count: %f", format(date, "%Y/%m/%d"), library, frequency))
+
+plot <- ggplot(interesting_sites, aes(x=date, y = frequency, color=library, text=text, group=1)) + 
+  geom_line(size=2) +
+  scale_color_manual(values=colors) +
+  scale_y_log10() +
+  facet_wrap(~client, ncol=1) +
+  ylab("Page count") +
+  ggtitle("Library Usage")
